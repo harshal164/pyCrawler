58df3304062ef128f238b14e77ce64fd15c4e763
J.A. Simmons V <simmonsj@jasimmonsv.com>
2013-01-22 14:49:33 -0600

    added image validation to solution

--- httpGet.py
+++ httpGet.py
@@ -16,7 +16,8 @@ masterTLD = ['.com','.edu','.org']
 masterFileFormat = ['.html','.htm','.php','.asp','.aspx','.jsp','.css','.cfm','.doc','.pdf']
 skipFileFormat = ['.mp3','.pdf']
 #define which servers to stay within - script will not crawl any pages outside these domains
-acceptedServers = ['redkeep.com','jasimmonsv.com']
+#acceptedServers = ['redkeep.com','jasimmonsv.com']
+acceptedServers = ['jasimmonsv.com','redkeep.com']
 #vars needed for cookie/session authentication
 CFID = '77514'
 CFTOKEN = '59455161'
@@ -24,7 +25,7 @@ CFTOKEN = '59455161'
 try:
     seed=sys.argv[1]
 except IndexError:
-    seed = 'http://jasimmonsv.com'
+    seed = 'http://jasimmonsv.com/'
 
 ##########################################################
 ###################CLASSES################################
@@ -39,6 +40,7 @@ class WebPage():
         self.reason = str()
         self.server, self.path = addressBreaker(initSeed)
         self.path = str()
+        self.brokenImgs = []
 
     #crawl_page function allows the WebPage class to crawl itself using multi-node tree logic
     #@input self self referrer
@@ -54,19 +56,18 @@ class WebPage():
             return
         print str(self.seed)
         if getDomain(self.seed) not in acceptedServers: #check that the page's server is on the acceptedServers list
-            passedSeed, content, self.status, self.reason = getURL(self.seed, True) #entire webpage, status code, status reason
-            crawled.append(self.seed)
+            content, self.status, self.reason = getURL(self.seed, True) #save status code and status reason
+            crawled.append(self.seed) #mark this page as crawled
             return #drop out of this current iteration
-        passedSeed, content, self.status, self.reason = getURL(self.seed) #entire webpage, status code, status reason
+        content, self.status, self.reason = getURL(self.seed) #entire webpage, status code, status reason
         crawled.append(self.seed) #add current page to crawled array
         if self.status == '200': #check that page was successfully retrieved.
             union(tempArray, get_all_links(content, self.seed)) #add all links within the page to a temp array
+            self.brokenImgs = get_broken_imgs(content, self.seed)
             for e in tempArray:
                 self.links.append(WebPage(e)) #add all links within temp array to self.links array
-            for e in range(len(self.links)):
-                if self.links[e] not in crawled:
-                    self.links[e].crawl_page(self) #depth-first crawl through self.links array
-                else: return
+            for e in range(len(self.links)): #crawl through all the links
+                self.links[e].crawl_page(self) #depth-first crawl through self.links array
         else: crawled.append(self.seed)
         return
     #printLinks function prints the links to the screen
@@ -89,13 +90,20 @@ class WebPage():
             e.savePages(n+1, f)
         f.write('</Page>\n')
         
-    def troubleReport(self, f):
+    def troubleReportLinks(self, f):
         for e in self.links:
             if (str(e.status) != '200'): 
                 if (str(e.status) != '100'):
                     f.write('"'+str(e.status)+'","'+self.seed+'","'+e.seed+'"\n')
             elif str(e.status) == '200':
-                e.troubleReport(f)                
+                e.troubleReportLinks(f)    
+
+    def troubleReportImgs(self, f):
+        for x in self.brokenImgs: 
+            f.write('"BrokenImg","'+self.seed+'","'+x+'"\n')
+        for e in self.links:
+            if str(e.status)=='200':
+                e.troubleReportImgs(f)            
 ##########################################################
 #########END CLASSES######################################
 ##########################################################
@@ -109,15 +117,13 @@ def getURL(page, passed=False):
     #build http connection
     opener = urllib2.build_opener()
     opener.addheaders.append(('Cookie','CFID='+CFID+'; CFTOKEN='+CFTOKEN))
-    #opener.addheaders.append(('User-agent','CMC Testing Web Crawling Daemon'))
-   
     #request connection
     try:
         request = urllib2.Request(page.replace(" ","%20"))
         request.add_header('User-agent','Testing Web Crawling Daemon/2.0')
         r1 = opener.open(request, timeout= 12)
         #get response
-        if str(r1.code) != '200': return page, -1, r1.code, r1.msg
+        if str(r1.code) != '200': return -1, r1.code, r1.msg
         #close the connection cleanly
         if page[-4:] in skipFileFormat:
             print 'Skipping downloading of '+page[-4:]+' file'
@@ -127,11 +133,11 @@ def getURL(page, passed=False):
     except Exception as inst:
         print " Error connecting to site: "+str(page)
         if type(inst) == urllib2.HTTPError:
-            return page, -1, inst.code, inst.msg
-        else: return page, -1, 0, inst
+            return -1, inst.code, inst.msg
+        else: return -1, 0, inst
     finally:
         opener.close()
-    return r1.geturl(), data, str(r1.code), r1.msg
+    return data, str(r1.code), r1.msg
 #Calls the get_next_target method to retrieve web links one link at a time, and saves 
 #to an array named links.
 #@input page
@@ -139,6 +145,7 @@ def getURL(page, passed=False):
 def get_all_links(page, ServerAdr):
     links=[]
     if page == -1:return links
+    #grab all the image links on the page
     for link in page.find_all('a'):
         passLink = link.get('href')
         if passLink !=None:
@@ -146,14 +153,32 @@ def get_all_links(page, ServerAdr):
             if url != -1: links.append(url)
     return links
 
-def get_all_imgs(page, ServerAdr):
+def get_broken_imgs(page, ServerAdr):
     links=[]
+    badImages=[]
     if page == -1:return links
     for link in page.find_all('img'):
         passLink = link.get('src')
         if passLink !=None:
             url = sanatizeURL(str(passLink),ServerAdr)
             if url != -1: links.append(url)
+    opener = urllib2.build_opener()
+    opener.addheaders.append(('Cookie','CFID='+CFID+'; CFTOKEN='+CFTOKEN))
+    for e in links:
+        #request connection and check status of images
+        try:
+            request = urllib2.Request(e.replace(" ","%20"))
+            request.add_header('User-agent','Testing Web Crawling Daemon/2.0')
+            r1 = opener.open(request, timeout= 12)
+            #get response
+            if str(r1.code) != '200': badImages.append(e) 
+            #close the connection cleanly
+        except Exception as inst:
+            print " Error connecting to site: "+str(e)
+            badImages.append(e)
+        finally:
+            opener.close()
+    return badImages
     return links
 
 
@@ -236,7 +261,8 @@ def output(rootPage):
     f.closed  
     with open('./openIssues.'+str(start)+'.csv','w') as f:
         f.write('"status","Parent","Link"\n')
-        rootPage.troubleReport(f)
+        rootPage.troubleReportLinks(f)
+        rootPage.troubleReportImgs(f)
     f.closed
 ########################################################<module>###################################################################
 
